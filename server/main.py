c9bc9b6292bf74089f9d3dc58309ab3da7422f11
Myron <myron.ilchenko@gmail.com>
2024-12-30 02:37:06 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -5,17 +5,17 @@ from server.websocket import router as websocket_router
 
 app = FastAPI()
 
-# Подключение маршрутов
+# Connecting routes
 app.include_router(auth.router, prefix="/auth")
 app.include_router(messages.router, prefix="/messages")
 app.include_router(websocket_router, prefix="")
 app.include_router(chats.router, prefix="/chats")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Разрешить доступ с любых доменов
+    allow_origins=["*"],  # Allow access from any domains
     allow_credentials=True,
-    allow_methods=["*"],  # Разрешить все методы (POST, GET, OPTIONS и т.д.)
-    allow_headers=["*"],  # Разрешить любые заголовки
+    allow_methods=["*"],  # Allow all methods (POST, GET, OPTIONS, etc.)
+    allow_headers=["*"],  # Allow any headers
 )
 
 @app.get("/")
