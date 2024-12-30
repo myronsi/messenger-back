278c13d6f0a60b4fce09cb3cafc57e86980b1069
Myron <myron.ilchenko@gmail.com>
2024-12-30 02:34:11 +0100

    Update connection_manager.py

--- server/connection_manager.py
+++ server/connection_manager.py
@@ -8,28 +8,28 @@ class ConnectionManager:
     async def connect(self, username: str, websocket: WebSocket):
         await websocket.accept()
         self.active_connections[username] = websocket
-        print(f"{username} подключён")
+        print(f"{username} connected")
 
     def disconnect(self, username: str):
         if username in self.active_connections:
             del self.active_connections[username]
-            print(f"{username} отключён")
+            print(f"{username} disabled")
 
     async def send_personal_message(self, message: str, username: str):
         if username in self.active_connections:
             websocket = self.active_connections[username]
             await websocket.send_text(message)
-            print(f"Сообщение отправлено {username}: {message}")
+            print(f"Message sent {username}: {message}")
 
     async def broadcast(self, message: str):
         for username, websocket in self.active_connections.items():
             await websocket.send_text(message)
-            print(f"Сообщение отправлено {username}: {message}")
+            print(f"Message sent {username}: {message}")
 
     def set_user_chat(self, username: str, chat_id: int):
-        """Установить активный чат для пользователя."""
+        """Set active chat for the user."""
         self.user_chats[username] = chat_id
 
     def get_user_chat(self, username: str) -> int:
-        """Получить текущий активный чат пользователя."""
+        """Get the user's currently active chat."""
         return self.user_chats.get(username)
